597edd9cac24c414af44c8ab1c281249bca727c1
BoiseState Student <student@u.boisestate.edu>
2021-11-30 20:28:12 -0700

    added nav bar and tested switching files

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <title>Hello World</title>
+</head>
+
+
+<body>  
+
+  <div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="newpost.php">Create Post</a>
+    <a href="editpost.php">Edit a Post</a>
+    <a class = "active" href="about.php">About</a>
+  </div>
+
+  <p>you are now in about.php</p>
+</body>
+  <script src="js/main.js"></script>
+</html>
--- index.php
+++ index.php
@@ -3,12 +3,21 @@
 
 <head>
   <link rel="stylesheet" href="css/style.css">
-  <title>Hello World</title>
+  <title>Ben's Basic Blog</title>
 </head>
 
 
 <body>  
-  <?php echo "<h1>Hello World! (CSS has changed this text to blue!)</h1>" ?>  
+
+  <div class="navbar">
+    <a class = "active" href="index.php">Home</a>
+    <a href="newpost.php">Create Post</a>
+    <a href="editpost.php">Edit a Post</a>
+    <a href="about.php">About</a>
+  </div>
+
+  <h1>Welcome to Ben's Basic Blog!</h1>
+  <h2>Here you can see all the latest posts!</h2>
   <img src="img/thedude.jpg" alt="The dude">
   <?php include("example.php"); ?>
   <button type="button" onclick="helloJS()">Click Me!</button>
